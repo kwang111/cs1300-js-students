9502c047ccaa20c3971d5c0c478fece545ec059a
Kelly Wang <kelly_wang1@brown.edu>
2020-10-28 18:32:35 -0700

    finish

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=S_xqkO1wsExK-UUQAWLc7FqMQgX2z1OyNTWTZpkJijY";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,56 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      handleResponse(request.response);
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const displayDiv = (content) => {
+  const wrapper = document.createElement("div");
+  wrapper.setAttribute("id", "wrapper");
+  const header = document.createElement("h1");
+  header.innerText = content;
+  wrapper.appendChild(header);
+  document.getElementById("plants").appendChild(wrapper);
+}
+
+const handleResponse = (requestResponse) => {
+  const jsonified = JSON.parse(requestResponse);
+  const plantArray = jsonified.data;
+
+  console.log(plantArray);
+
+  const filteredPlants = plantArray.filter((arrayItem) => {
+    return arrayItem.family_common_name == "Rose family";
+  })
+
+console.log(filteredPlants);
+
+filteredPlants.map((plant)=>{
+  const wrapper = document.createElement("div");
+  const name = document.createElement("h2");
+  const image = document.createElement("img");
+  image.setAttribute("src", plant.image_url);
+  wrapper.setAttribute("margin", "5%");
+  name.innerText = plant.common_name;
+  wrapper.appendChild(name);
+  wrapper.appendChild(image);
+  document.getElementById("plants").appendChild(wrapper);
+});
+
+}
+
+/*  plantsAfter1753.map(console.log) */
+
+/* const handleResponse = (requestResponse) => {
+  const jsonified = JSON.parse(requestResponse);
+  const plantsArray = jsonified.data;
+
+  const plantsAfter1753 = plantsArray.filter((arrayItem) => {
+    return arrayItem.year > 1753;
+  })
+
+  plantsAfter1753.map(console.log)
+}
+ */
